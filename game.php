70b28992092884ddeaa536c4c13f5f4c01c2db73
bhavikj06 <bhavikj06@users.noreply.github.com>
2020-06-10 20:51:22 +0530

    Create game.php

---
+++ game.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>first php page</title>
+</head>
+<body>
+	<h1>$_GET</h1>
+	<p>using print_r</p>
+	<pre>
+		<?php 
+			print_r($_GET);
+		?>
+	</pre>
+	<p> using var_dump</p>
+	<pre>
+		<?php 
+			var_dump($_GET);
+		?>
+	</pre>
+</body>
+</html>
